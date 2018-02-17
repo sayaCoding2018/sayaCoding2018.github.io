152873b34686616d8715e577ce15cfd95ee768e0
Gautam <gautam.sisodia@gmail.com>
2018-02-17 17:15:20 -0500

    djaskj

--- sketch.js
+++ sketch.js
@@ -4,5 +4,17 @@ function setup() {
 
 
 function draw() {
+  background(217, 95, 2);
+
+  stroke(0, 0, 255);
+
+  if (mouseX > 300) {
+    fill(0, 255, 0);
+  }
+  else {
+    fill(0, 0, 255);
+  }
+
+  strokeWeight(5);
   ellipse(50, 50, 50, 50);
 }
